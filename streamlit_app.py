3e0f7b99dc05210cfc7126cff59b6f9784955459
magic9145 <38551655+magic9145@users.noreply.github.com>
2023-03-09 13:13:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,20 @@
-###########
-# sidebar #
-###########
-option = st.sidebar.selectbox('Select one symbol', ( 'AAPL', 'MSFT',"SPY",'WMT'))
-import datetime
-today = datetime.date.today()
-before = today - datetime.timedelta(days=700)
-start_date = st.sidebar.date_input('Start date', before)
-end_date = st.sidebar.date_input('End date', today)
-if start_date < end_date:
-    st.sidebar.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
-else:
-    st.sidebar.error('Error: End date must fall after start date.')
+import numpy as np
+import plotly.figure_factory as ff
+import streamlit as st
 
-    
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
